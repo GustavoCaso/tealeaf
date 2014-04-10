26fe898a581f9f17f568013909c5fe4ff7232833
GustavoCaso <gustavocaso@gmail.com>
2014-04-10 20:54:08 +0200

    * imporved some code in blackjack

--- Lesson_1/black_jack.rb
+++ Lesson_1/black_jack.rb
@@ -32,48 +32,54 @@ def dealer_turn(dealer_cards,cards_shuffle,player_hand)
     dealer_turn(dealer_cards,cards_shuffle,player_hand)
   elsif (dealer_hand > player_hand || dealer_hand == player_hand) && dealer_hand < 21
     say "Dealer hand is #{dealer_hand} and your is #{player_hand} Dealer stay and wins"
-    exit
+    re_game
   elsif dealer_hand > 21
     say "Dealer hand is #{dealer_hand} Is way to high player wins"
-    exit
-  elsif delaer_hand == 21
-    say "The Dealet has BlackJack Dealer Wins"
-    exit
+    re_game
+  elsif dealer_hand == 21
+    say "The Dealer has BlackJack Dealer Wins"
+    re_game
   end
 end
 
 
 
-def play(players_cards,cards_shuffle)
+def play(player_name,players_cards,cards_shuffle)
   player_hand, ace = evaluate_cards(players_cards)
+  binding.pry
   if player_hand == 21
     say "You have blackJack You Win"
-    say "Congratulations"
-    exit
+    say "Congratulations #{player_name}"
+    re_game
   elsif player_hand > 21
     say "You have more that 21 I'm sorry but the house wins"
-    say "Sorry"
-    exit
+    say "Sorry #{player_name}"
+    re_game
   else
     if ace
-      say "You hace an Ace and your game is #{player_hand} but it could be #{player_hand - 10} what do you want"
-      player_hand = gets.chomp.to_i
+      say "#{player_name} you have an Ace and your game is #{player_hand} but it could be #{player_hand - 10} what do you want"
+      choice = gets.chomp.to_i
+      while choice != player_hand && choice != player_hand -10
+        say "Don't try to cheat, be nice, so what would be your choice"
+        choice = gets.chomp.to_i
+      end
+      player_hand = choice
       say "Ok so you have #{player_hand}, what do you want to do (hit or stay)"
       answer = gets.chomp.downcase
       while answer == "hit"
         players_cards << deal_cards(1, cards_shuffle).first
-        play(players_cards, cards_shuffle)
+        play(player_name,players_cards, cards_shuffle)
       end
       dealer_cards = dealer_cards = deal_cards(2, cards_shuffle)
       dealer_turn(dealer_cards,cards_shuffle,player_hand)
 
 
     else
-      say "you have #{player_hand} what you want to do? (hit or stay)"
+      say "#{player_name} your hand is #{player_hand} what you want to do? (hit or stay)"
       answer = gets.chomp.downcase
       while answer == "hit"
         players_cards << deal_cards(1, cards_shuffle).first
-        play(players_cards, cards_shuffle)
+        play(player_name,players_cards, cards_shuffle)
       end
       dealer_cards = dealer_cards = deal_cards(2, cards_shuffle)
       dealer_turn(dealer_cards,cards_shuffle,player_hand)
@@ -101,14 +107,29 @@ def evaluate_cards(player_cards, game = 0)
   [game,ace]
 end
 
-
-
-def start_game(player ,player_cards, cards_shuffle)
-  say "are yoy ready to start (yes or no)"
+def re_game
+  say "Would you like to play again ?"
   answer = gets.chomp.downcase
   if answer == "yes"
-    say "great what do you want to do #{player}"
-    play(player_cards, cards_shuffle)
+    start_game
+  else
+    say "It's been great hope to see you later"
+    exit
+  end
+end
+
+
+
+def start_game
+  cards = create_cards
+  cards_shuffle = shuffle cards
+  player_cards = deal_cards(2, cards_shuffle)
+  say "What's your name"
+  player_name = gets.chomp
+  say "Are you ready to start? (yes or no)"
+  answer = gets.chomp.downcase
+  if answer == "yes"
+    play(player_name, player_cards, cards_shuffle)
   else
     say "really I will ask you until you say yes #{player}"
     start_game(player ,player_cards, cards_shuffle)
@@ -120,15 +141,9 @@ end
 
 
 say "Hy and welcome to the world best BlackJack ever. Do you want to play, I'm sure you want. First I'm going to shuffle the cards"
-say "what is your name"
-player_1 = gets.chomp
-say "Hey #{player_1} this are the cards"
-cards = create_cards
-say "know I'm going to shuffle them"
-cards_shuffle = shuffle cards
-say "Let the game begin"
-player_cards = deal_cards(2, cards_shuffle)
-start_game(player_1, player_cards, cards_shuffle)
+
+
+start_game
 
 
 
