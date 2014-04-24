c32b3ae95b7b0a07b00fec047e3f33ae8a188259
GustavoCaso <gustavocaso@gmail.com>
2014-04-24 17:15:41 +0200

    Working sinatra

--- Lesson_3/main.rb
+++ Lesson_3/main.rb
@@ -6,15 +6,65 @@ require 'pry'
 set :sessions, true
 
 helpers do
+
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+
+  def show_cards(cards)
+    url = []
+    cards.each do |card|
+      image_name = card.join('_')
+      url << "images/cards/#{image_name}.jpg"
+    end
+    url
+  end
+
   def create_deck
-    suit = ["clubs", "diamonds", "herts", "spades"]
+    suit = ["clubs", "diamonds", "hearts", "spades"]
     cards = %w( 2 3 4 5 6 7 8 9 10 jack queen king ace)
     deck = suit.product(cards)
+    deck.shuffle!
   end
 
   def add_card(deck)
     deck.pop
   end
+
+  def calculate_total(cards)
+    total = 0
+    number = []
+    cards.each do |card|
+      number << card[1]
+    end
+
+    number.each do |n|
+      if n == "ace"
+        total += 11
+      elsif n.to_i == 0
+        total += 10
+      else
+        total += n.to_i
+      end
+    end
+
+    number.select{ |n| n == "ace"}.count.times do
+      total -= 10 if total > 21
+    end
+    total
+  end
+
+  def evaluate_game(game)
+    if game == 21
+      "The player has blackJack congratulations"
+    elsif game > 21
+      "The player is busted with #{game}"
+    else
+      "What would you like to do"
+    end
+  end
+
+
 end
 
 get '/' do
@@ -38,6 +88,7 @@ post '/start_game' do
   session[:player_cards] = []
   session[:player_cards] << add_card(session[:deck])
   session[:player_cards] << add_card(session[:deck])
+  session[:player_total] = calculate_total(session[:player_cards])
   haml :start_game
 end
 
